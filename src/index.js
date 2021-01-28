4a10a20a57a14f28f48c2fe4beef7e91e3031d50
michelangelo7 <m_che_l@hotmail.com>
2021-01-28 13:42:28 -0300

    calculations example

--- src/index.js
+++ src/index.js
@@ -9,8 +9,45 @@ function addRestatement(){
 
     document.getElementById("showStartDate").innerHTML = startDate
 
-        document.getElementById("showDebit").innerHTML = debit
+    console.log(interestPerMonth(startDate))
+
+    document.getElementById("showDebit").innerHTML = debit
 
     document.getElementById("showPaid").innerHTML = paid 
 
+    let difference = debit - paid
+
+    document.getElementById("showDifference").innerHTML = difference
+
+    let monetaryCorrection = 1.563203
+
+    document.getElementById("showMonetaryCorrection").innerHTML = monetaryCorrection
+
+    let correctedValue = difference * monetaryCorrection
+
+    document.getElementById("showCorrectedValue").innerHTML = correctedValue
+
+    let interestPerMonthCalc = interestPerMonth(startDate)
+
+    document.getElementById("showInterestPerMonth").innerHTML = interestPerMonthCalc
+
+
+    let interest = correctedValue * interestPerMonthCalc
+
+    document.getElementById("showInterest").innerHTML = interest
+
+    let finalValue = interestPerMonthCalc + interest
+
+    document.getElementById("showFinalValue").innerHTML = finalValue
+}
+
+
+function interestPerMonth(startDate){
+    let now = new Date() 
+    let past = new Date(startDate)
+    let diff = Math.abs(now.getTime() - past.getTime())
+    let days = Math.ceil(diff / (1000 * 60 * 60 * 24))
+    let month = Math.floor((days/30))
+
+    return month
 }
